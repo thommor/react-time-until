2092c42e3b93ace312c255c199fa14508ff3fb8b
Griffin Roupe <groupe@renci.org>
2022-08-21 21:16:09 -0400

    Improvements

--- src/time-until.tsx
+++ src/time-until.tsx
@@ -1,16 +1,27 @@
 /**
  * Countdown display component
  */
-import React, { Fragment } from 'react'
+import React, { Fragment, ReactNode } from 'react'
 import { useTimeUntil, UseTimeUntilProps } from './use-time-until'
 
 type TimeUntilProps = {
-    format?: "text" | "time",
-    // Appends "ago" when the time delta is negative
+    format?: "text" | "time"
+    // Prepends "in"/appends "ago"
+    // Only supported for "text" format
     ago?: boolean
+    inText?: string
+    agoText?: string
+    finishText?: ReactNode
 } & UseTimeUntilProps
-export const TimeUntil = ({ format="time", ago=false, ...props }: TimeUntilProps) => {
-    const { delta, seconds, minutes, hours } = useTimeUntil(props)
+export const TimeUntil = ({
+    format="text",
+    ago=false,
+    inText="in ",
+    agoText=" ago",
+    finishText=undefined,
+    ...props
+}: TimeUntilProps) => {
+    const { delta, seconds, minutes, hours, finished } = useTimeUntil(props)
 
     const hoursText = hours !== 1 ? `${ hours } hours` : `1 hour`
     const minutesText = minutes !== 1 ? `${ minutes } minutes` : `1 minute`
@@ -19,9 +30,14 @@ export const TimeUntil = ({ format="time", ago=false, ...props }: TimeUntilProps
     const hoursTimeString = hours === 1 ? hours : hours.toString().padStart(2, "0")
     const minutesTimeString = hours === 0 && minutes === 1 ? minutes : minutes.toString().padStart(2, "0")
     const secondsTimeString = seconds.toString().padStart(2, "0")
-
+    if (props.countdown && finishText && finished) return (
+        <Fragment>
+            { finishText }
+        </Fragment>
+    )
     if (format === "text") return (
         <Fragment>
+            { ago && delta >= 0 && inText }
             { hours > 0 ? (
                 `${ hoursText } and ${ minutesText }`
             ) : minutes > 0 ? (
@@ -29,7 +45,7 @@ export const TimeUntil = ({ format="time", ago=false, ...props }: TimeUntilProps
             ) : (
                 `${ secondsText }`
             ) }
-            { ago && delta < 0 && " ago" }
+            { ago && delta < 0 && agoText }
         </Fragment>
     )
     if (format === "time") return (
--- src/use-time-until.ts
+++ src/use-time-until.ts
@@ -3,45 +3,70 @@ import { useEffect, useMemo, useState } from 'react'
 type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never }
 type XOR<T, U> = (T | U) extends object
   ? (Without<T, U> & U) | (Without<U, T> & T)
-  : T | U;
+  : T | U
+  
 interface BaseProps {
     // Update interval
     interval?: number
 }
-export type UseTimeUntilProps = BaseProps & XOR<{
+interface CountdownProps {
+    // Countdown will stop at 0 instead of going into the negatives
+    countdown?: boolean
+    onFinish?: () => void
+}
+export type UseTimeUntilProps = BaseProps & CountdownProps & XOR<{
     time: Date
 }, {
     delta: number
 }>
 
-export const useTimeUntil = ({ time, delta, interval=50 }: UseTimeUntilProps): {
+export interface TimeUntilValue {
     delta: number,
     seconds: number,
     minutes: number,
-    hours: number
-} => {
+    hours: number,
+    finished: boolean
+}
+
+export const useTimeUntil = ({
+    time,
+    delta,
+    countdown=false,
+    onFinish=() => {},
+    interval=50
+}: UseTimeUntilProps): TimeUntilValue => {
     const [currentTime, setCurrentTime] = useState<number>(Date.now())
 
     const targetTime = useMemo<number>(() => (
         time ? time.getTime() : new Date(Date.now() + delta!).getTime()
     ), [time, delta])
 
-    // MS until time is reached as an epoch Date
-    const timeDeltaNumber = useMemo<number>(() => targetTime - currentTime, [targetTime, currentTime])
+    // Delta until targetTime is reached
+    const timeDeltaNumber = useMemo<number>(() => (
+        countdown ? Math.max(0, targetTime - currentTime) : targetTime - currentTime
+    ), [targetTime, currentTime, countdown])
+    // MS until targetTime is reached as an epoch Date
     const timeDelta = useMemo<Date>(() => {
-        const rounder = timeDeltaNumber > 0 ? Math.floor : Math.ceil
+        const rounder = timeDeltaNumber > 0 ? Math.ceil : Math.floor
+        // const rounder = Math.ceil
         return new Date(
             rounder(
                 Math.abs(timeDeltaNumber) / 1000
             ) * 1000
         )
     }, [timeDeltaNumber])
+
+    const finished = useMemo(() => (
+        timeDeltaNumber <= 0
+    ), [timeDeltaNumber])
+
     const timeUntil = useMemo(() => ({
         delta: timeDeltaNumber,
         seconds: timeDelta.getUTCSeconds(),
         minutes: timeDelta.getUTCMinutes(),
-        hours: timeDelta.getUTCHours()
-    }), [timeDelta, timeDeltaNumber])
+        hours: timeDelta.getUTCHours(),
+        finished
+    }), [timeDelta, timeDeltaNumber, finished])
 
     useEffect(() => {
         const updater = window.setInterval(() => {
@@ -51,6 +76,10 @@ export const useTimeUntil = ({ time, delta, interval=50 }: UseTimeUntilProps): {
             clearInterval(updater)
         }
     }, [interval])
+
+    useEffect(() => {
+        if (countdown && finished) onFinish()
+    }, [countdown, finished])
 
     return timeUntil
 }
